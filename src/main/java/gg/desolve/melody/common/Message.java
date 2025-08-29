71427b9438bc52af1c3cad93427b57cd1a179108
michele <michelegje@gmail.com>
2025-08-30 00:30:57 +0100

    Improved message utils

--- src/main/java/gg/desolve/melody/common/Message.java
+++ src/main/java/gg/desolve/melody/common/Message.java
@@ -1,11 +1,24 @@
 package gg.desolve.melody.common;
 
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.entity.Player;
 
 public class Message {
 
+    private static final MiniMessage MM = MiniMessage.miniMessage();
+    private static final LegacyComponentSerializer LEGACY = LegacyComponentSerializer.legacySection();
+
+    public static Component translate(String message) {
+        return MM.deserialize(message);
+    }
+
+    public static String translateLegacy(String message) {
+        return LEGACY.serialize(MM.deserialize(message));
+    }
+
     public static void sendMessage(Player player, String message) {
-        player.sendMessage(MiniMessage.miniMessage().deserialize(message));
+        player.sendMessage(translateLegacy(message));
     }
 }
